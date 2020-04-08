ec4452b453141f69308f249a4b9bf631a114eb93
Mattia541993 <ducci.mattia@hotmail.it>
2020-04-08 21:31:32 +0200

    tolte sleep per adattarlo al caso real time

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ app = Flask(__name__)
 CORS(app) #di default CORS *
 nlp = it_core_news_sm.load()
 keyboard = Controller()
+#fetch resources
 with open('./util/word2num.json') as word2num_json:
     word2numDict = json.load(word2num_json)
 with open('./util/derivata_word2num.json') as derivata_word2num_json:
@@ -33,6 +34,7 @@ moduleAskingNewLayer = {'module_name':None} #per sapere a che grammatica reindir
 ruleAskingNewLayer = {'rulename':None} #servirà al modulo per sapere come muovere il cursore di conseguenza. Da rispedire indietro quando il top layer ha finito
 lastTextSent = {'text':''} #tiene conto dell'ultimo testo inviato a texstudio
 numBurstTokens = {'length':-1}
+lastSaveTime = {'last_save_time':time.time()}
 
 layer = Layer()
 
@@ -53,6 +55,7 @@ def manageLayerAnswer(layerAnswer):
                 pressSpaceTimes(eventualEndCursorMovement)
             else:
                 muoviCursoreIndietroDi(abs(eventualEndCursorMovement))
+        # salvaTex()
             
         app.logger.debug('txt for texstudio: {}'.format(txtToSend))
         """Aggiornamento stato"""
@@ -78,35 +81,6 @@ def manageLayerAnswer(layerAnswer):
 
     elif layerAnswer[0] == LayerMsg.END_THIS_LAYER_WITH_TEXT:
         pass
-        # if isinstance(layerAnswer[1],dict): #vuol dire che c'è parte del burst che ha prodotto una foglia, mentre il resto è da mandare dentro \text{}. A seguito di un esplicito comando 'fine'
-        #     txtToSend = layerAnswer[1]['tag']
-        #     lastGoodIdx = layerAnswer[1]['idx']
-        #     freeText = '\\text{{{0}}}'.format(curBurst['tokens'][lastGoodIdx:])
-        #     keyboard.type(txtToSend)
-        #     app.logger.debug('txt for texstudio: {}'.format(txtToSend))
-        #     keyboard.type(freeText)
-        #     app.logger.debug('txt for texstudio as plain text: {}'.format(freeText))
-        #     """Aggiornamento stato"""
-        #     lastTextSent['text'] = freeText
-        #     lastAction['action'] = Action.DETTATURA
-        # else: #il messaggio di fine layer arriva con un testo semplice come payload
-        #     txtToSend = layerAnswer[1]
-        #     keyboard.type(txtToSend)
-        #     app.logger.debug('txt for texstudio: {}'.format(txtToSend))
-        #     """Aggiornamento stato"""
-        #     lastTextSent['text'] = txtToSend
-        #     lastAction['action'] = Action.DETTATURA
-        # allTextSentByTopLayer = stack[-1].allTextSent #prendo tutto quanto detto nel top-layer
-        # stack.pop() #fine layer
-        # if len(stack) > 0: #se quello che ho appena tolto non era l'unico layer
-        #     eventualCursorMovement = stack[-1].updateGrammarStringFormat(allTextSentByTopLayer,moduleAskingNewLayer['module_name'],ruleAskingNewLayer['rulename'])
-        #     if eventualCursorMovement is not None and eventualCursorMovement != 0:
-        #         if eventualCursorMovement > 0:
-        #             keyboard.type('__mf{}'.format(eventualCursorMovement))
-        #         else:
-        #             keyboard.type('__mb{}'.format(eventualCursorMovement))
-        # """Aggiornamento stato"""
-        # resetPrevLayerStatusVars()
 
     elif layerAnswer[0] == LayerMsg.NEW_LAYER_REQUEST:
         # pdb.set_trace()
@@ -121,48 +95,22 @@ def manageLayerAnswer(layerAnswer):
         if tag is not None:
             keyboard.type(tag)
             muoviCursoreIndietroDi(len(tag)) #RITORNO A INIZIO COMANDO (perchè l'offset lo do rispetto a quello)
+            # salvaTex()
             # keyboard.type('__mb{}'.format(len(tag))) 
         else:
             if carryHomeLength is not None:
                 carryHomeLength = int(carryHomeLength)
                 if carryHomeLength > 0:
                     muoviCursoreIndietroDi(carryHomeLength)
-                    # if carryHomeLength > 9: #la regex di texstudio prende una sola cifra, quindi spezzetto uno spostamento del cursore lungo in tanti piccoli
-                    #     tmp_cnt = carryHomeLength
-                    #     while tmp_cnt > 9:
-                    #         time.sleep(1)
-                    #         keyboard.type('__mb9')
-                    #         tmp_cnt -= 9
-                    #     time.sleep(1)
-                    #     keyboard.type('__mb{}'.format(tmp_cnt))
-                    # else:
-                    #     time.sleep(1)
-                    #     keyboard.type('__mb{}'.format(carryHomeLength))
                 else: #se carry home length è negativo
                     muoviCursoreAvantiDi(abs(carryHomeLength))
-                    # if abs(carryHomeLength) > 9:
-                    #     tmp_cnt = abs(carryHomeLength)
-                    #     while tmp_cnt > 9:
-                    #         time.sleep(1)
-                    #         keyboard.type('__mf9')
-                    #         tmp_cnt -= 9
-                    #     time.sleep(1)
-                    #     keyboard.type('__mf{}'.format(tmp_cnt))
-                    # time.sleep(1)
-                    # keyboard.type('__mf{}'.format(abs(carryHomeLength)))
-        # pdb.set_trace()
         
         if cursorOffset > 0:
             #dico a texstudio di andare avanti col cursore
             muoviCursoreAvantiDi(cursorOffset)
-            # time.sleep(1)
-            # keyboard.type('__mf{}'.format(cursorOffset))
         else: #strettamente minore di 0
             #dico a texstudio di andare indietro col cursore
             muoviCursoreIndietroDi(abs(cursorOffset))
-            # time.sleep(1)
-            # keyboard.type('__mb{}'.format(cursorOffset))
-        # stack.append(Layer())
         """Aggiornamento stato"""
         lastTextSent['text'] = tag if tag != None else lastTextSent['text']
         lastAction['action'] = Action.DETTATURA
@@ -176,11 +124,11 @@ def manageLayerAnswer(layerAnswer):
                 pressSpaceTimes(eventualEndCursorMovement)
             else:
                 muoviCursoreIndietroDi(abs(eventualEndCursorMovement))
+        # salvaTex()
         idx_start = layerAnswer[1]
         for i in range(idx_start,len(curBurst['tokens'])): #non vado oltre dove sono già arrivato
-            # newLayerIfNeeded()
             res = layer.handleRawText((curBurst['tokens'][i],'NA'),i,numBurstTokens['length'])
-            time.sleep(2) #per debug. Per darmi tempo di switchare su texstudio
+            #time.sleep(2) #per debug. Per darmi tempo di switchare su texstudio
             manageLayerAnswer(res)
 
 
@@ -192,8 +140,6 @@ def new_text():
     curBurst['tokens'] = []
     doc = nlp(last_burst)
     numBurstTokens['length'] = len(doc)
-
-    # newLayerIfNeeded()
     
     for idx,token in enumerate(doc):
         tokenText = ''
@@ -227,22 +173,22 @@ def new_text():
                 if nextRule[0] != LayerMsg.NEW_LAYER_REQUEST: #altrimenti lo spostamento lo gestisco già in manageLayerAnswer
                     if eventualCursorMovement is not None and eventualCursorMovement != 0:
                         if eventualCursorMovement > 0:
-                            time.sleep(1)
+                            #time.sleep(1)
                             keyboard.type('__mf{}'.format(eventualCursorMovement))
                         else:
-                            time.sleep(1)
+                            #time.sleep(1)
                             keyboard.type('__mb{}'.format(eventualCursorMovement)) 
                 stack.pop()
                 manageLayerAnswer(nextRule)
             else:
                 res = layer.handleRawText((tokenText,tokenPos),idx,numBurstTokens['length'])
-                time.sleep(2) #per debug. Per darmi tempo di switchare su texstudio
+                #time.sleep(2) #per debug. Per darmi tempo di switchare su texstudio
                 manageLayerAnswer(res)
         else:
             # else: #questo token mi fa restare su questo layer
             # newLayerIfNeeded()
             res = layer.handleRawText((tokenText,tokenPos),idx,numBurstTokens['length'])
-            time.sleep(2) #per debug. Per darmi tempo di switchare su texstudio
+            #time.sleep(2) #per debug. Per darmi tempo di switchare su texstudio
             manageLayerAnswer(res)
     return '',status.HTTP_200_OK
 
@@ -260,18 +206,18 @@ def muoviCursoreAvantiDi(num_caratteri):
     if num_caratteri > 9:
         tmp_cnt = abs(num_caratteri)
         while tmp_cnt > 9:
-            time.sleep(1)
+            #time.sleep(1)
             keyboard.type('__mf9')
             tmp_cnt -= 9
-        time.sleep(1)
+        #time.sleep(1)
         keyboard.type('__mf{}'.format(tmp_cnt))
     else:
-        time.sleep(1)
+        #time.sleep(1)
         keyboard.type('__mf{}'.format(abs(num_caratteri)))
 
 def pressSpaceTimes(times):
     for i in range(0,times):
-        time.sleep(1)
+        #time.sleep(1)
         keyboard.press(Key.space)
 
 def muoviCursoreIndietroDi(num_caratteri):
@@ -280,14 +226,19 @@ def muoviCursoreIndietroDi(num_caratteri):
     if num_caratteri > 9:
         tmp_cnt = abs(num_caratteri)
         while tmp_cnt > 9:
-            time.sleep(1)
+            #time.sleep(1)
             keyboard.type('__mb9')
             tmp_cnt -= 9
-        time.sleep(1)
+        #time.sleep(1)
         keyboard.type('__mb{}'.format(tmp_cnt))
     else:
-        time.sleep(1)
+        #time.sleep(1)
         keyboard.type('__mb{}'.format(abs(num_caratteri)))
+
+# def salvaTex():
+#     if time.time() - lastSaveTime['last_save_time'] > 1: #salvo solo se l'ultima volta ho salvato più di un secondo fa
+#         keyboard.type('__save')
+#         lastSaveTime['last_save_time'] = time.time()
 
 
 
